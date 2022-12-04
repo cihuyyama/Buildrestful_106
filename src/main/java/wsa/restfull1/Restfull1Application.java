dd4928b4eb3b92aa1626e229ad45cc8ed1f59b7d
cihuyyama <miqbalalhabib@gmail.com>
2022-12-04 13:03:17 +0700

    pushing the last project

---
+++ src/main/java/wsa/restfull1/Restfull1Application.java
@@ -0,0 +1,13 @@
+package wsa.restfull1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Restfull1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Restfull1Application.class, args);
+	}
+
+}
---
+++ src/test/java/wsa/restfull1/Restfull1ApplicationTests.java
@@ -0,0 +1,13 @@
+package wsa.restfull1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Restfull1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
